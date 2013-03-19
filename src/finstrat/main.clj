c239678a9da9f28ae6d59bb2e431ca8c9543253c
Timothy Pratley <timothypratley@gmail.com>
2013-03-18 18:44:28 -0700

    Comparison to interest rates and inverse strategy

--- src/finstrat/main.clj
+++ src/finstrat/main.clj
@@ -1,37 +1,93 @@
 (ns finstrat.main
   (:use [clojure-csv.core]))
 
-;; Cut losses, run profits (Momentum)
+;; Run profits and cut losses (Momentum)
 ;; state machine start in waiting, if x up -> bought if x down -> waiting
-(defn assoc-if
-  [comparer new-value old-value]
-  (if (comparer new-value old-value)
-    new-value
-    old-value))
 
-(def tollerance 3)
-(defn step [state price]
-  (let [state (assoc state :low (min price (state :low)))
-        state (assoc state :high (max price (state :high)))]
+(declare init buy sell update)
 
-    (if (= (state :name) :waiting)
-      (if (> (- price (state :low)) tollerance)
-        (assoc state
-               :name :holding
-               :price price)  ;bought
-        state)
-      (if (> (- (state :high) price) tollerance)
-        (assoc state
-               :name :waiting
-               :acc (+ (state :acc) (- price (state :price))))  ;sold
-        state))))
+(def tollerance 0.03)
+(defn up
+  [preferences state price]
+  (> price (+ (state :low) (* (state :low) (preferences :tollerance)))))
+(defn down
+  [preferences state price]
+  (< price (- (state :high) (* (state :high) (preferences :tollerance)))))
+  
+(defn step [preferences up down state price]
+  (comment DEBUG println "STEP: " state price)
+  (cond
+    (nil? state)
+    (init state price)
 
+    (= (state :name) :waiting)
+    (if (up preferences state price)
+      (buy state price)
+      (update state price))
+
+    (= (state :name) :holding)
+    (if (down preferences state price)
+      (sell state price)
+      (update state price))
+
+    :else
+      (throw (Exception. "Invalid state name"))))
+
+(defn data
+  []
+  (map (comp read-string second)
+       (reverse (parse-csv (slurp "realsap.csv")))))
+
+;; TODO: plot them
 (defn -main [& m]
-  (let [s (map (comp read-string second) (reverse (parse-csv (slurp "realsap.csv"))))
-        price (first s)
-        state {:name :waiting
-               :high price
-               :low price
-               :acc 0}]
-    (println (reduce step state s))))
+  (let [prices (data)
+        state nil
+        inflation 0.023
+        ;all (reductions step state prices)
+        ]
+    (doseq [i [0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10]]
+      (println "Compound interest " i " - inflation " inflation " => " 
+               (Math/pow (+ 1 (- i inflation)) (/ (count prices) 12))))
+    (println "HELD FOR DURATION: " (/ (last prices) (first prices)))
+    (doseq [tollerance [0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10]]
+      (let [result (reduce (partial step {:tollerance tollerance} up down) state prices)]
+        (println "POSITIVE " tollerance ": "
+                   (+ (result :cash) (* (last prices) (result :shares))))))
+    (doseq [tollerance [0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10]]
+      (let [result (reduce (partial step {:tollerance tollerance} down up) state prices)]
+        (println "NEGATIVE " tollerance ": "
+                   (+ (result :cash) (* (last prices) (result :shares))))))))
+
+(defn update
+  [state price]
+  (-> state
+    (update-in [:low] (partial min price))
+    (update-in [:high] (partial max price))))
+
+;; TODO: protect against init occuring multiple times
+(defn init
+  [state price]
+  {:name :waiting
+   :high price
+   :low price
+   :cash 1
+   :shares 0})
+
+(defn buy
+  [state price]
+  (assoc state
+    :name :holding
+    :low price
+    :high price
+    :cash 0
+    :shares (/ (state :cash) price)))
+
+(defn sell
+  [state price]
+  (assoc state
+    :name :waiting
+    :low price
+    :high price
+    :cash (+ (state :cash) (* price (state :shares)))
+    :shares 0))
 
